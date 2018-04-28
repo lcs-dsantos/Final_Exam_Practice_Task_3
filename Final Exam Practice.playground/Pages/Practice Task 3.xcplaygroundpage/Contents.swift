32776616c42bda3c45c2a197fdf9c76ad8486ded
Hannah Hofmann <hhofmann@lcs.on.ca>
2018-04-28 11:35:53 -0400

    bleh

--- Final Exam Practice.playground/Pages/Practice Task 3.xcplaygroundpage/Contents.swift
+++ Final Exam Practice.playground/Pages/Practice Task 3.xcplaygroundpage/Contents.swift
@@ -30,7 +30,94 @@ let canvas = Canvas(width: 400, height: 600)
 let lightGrey = Color(hue: 84, saturation: 6, brightness: 88, alpha: 100)
 let deepRed = Color(hue: 7, saturation: 97, brightness: 72, alpha: 100)
 
-// Begin your solution here... 
+// Begin your solution here...
+
+//background
+
+    canvas.fillColor = lightGrey
+
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
+
+//group A of text
+
+    canvas.textColor = deepRed
+
+    canvas.drawText(message: "straight music presents", size: 10, x: 13, y: 550, kerning: -0.5)
+
+    canvas.drawText(message: "the undertones", size: 10, x: 13, y: 540, kerning: -0.5)
+
+
+
+    canvas.drawText(message: "with special guests", size: 10, x: 13, y: 515, kerning: -0.5)
+
+
+    canvas.drawText(message: "the chords", size: 10, x: 13, y: 505, kerning: -0.5)
+
+//gorup B of text
+
+    canvas.drawText(message: "victoria hall hanley", size: 10, x: 150, y: 550, kerning: -0.5)
+
+    canvas.drawText(message: "stoke-on-trent, england", size: 10, x: 150, y: 540, kerning: -0.5)
+
+
+    canvas.drawText(message: "tuesday", size: 10, x: 150, y: 515, kerning: -0.5)
+
+    canvas.drawText(message: "june 5 1979 / 7:30 pm", size: 10, x: 150, y: 505, kerning: -0.5)
+
+//group C of text
+
+    canvas.drawText(message: "advance tickets: £2.00", size: 10, x: 280, y: 550, kerning: -0.5)
+
+    canvas.drawText(message: "at the door: £2.50", size: 10, x: 280, y: 540, kerning: -0.5)
+
+
+for x in stride(from: 0, to: 400, by: 50){
+    canvas.lineColor = Color.white
+    canvas.drawLine(fromX: x, fromY: 0, toX: x, toY: 600)
+}
+
+for y in stride(from: 0, to: 600, by: 50){
+    canvas.drawLine(fromX: 0, fromY: y, toX: 400, toY: y)
+}
+
+
+
+//move the origin
+
+canvas.translate(byX: 13, byY: 400)
+
+    //fade loop
+    
+    for a in stride(from: 100, to: 0, by: -3){
+        
+        canvas.textColor = Color.init(hue: 0, saturation: 0, brightness: 0, alpha: a)
+        
+        //undertones logo
+        
+        canvas.drawText(message: "undertones", size: 70, x: 0, y: 0, kerning: 0)
+        
+        //rotate
+        
+        canvas.rotate(by: -5)
+        
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*:
  **Remember to commit and push your work,please**.
